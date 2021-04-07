213a71c96b2b09d7971c3eebf5f18f2e637295b5
Alex Tam <alextam@adam.com.au>
2021-04-07 12:43:12 +1000

    Update optimise_network.jl

--- optimise_network.jl
+++ optimise_network.jl
@@ -4,7 +4,7 @@
 "Minimise energy functional to solve model"
 function optimise_network(s_old, af, xl, mm, parN, parA, parM, Lxx, Lxy, Lyx, Lyy)
     dof = build_dof(s_old); # Construct DOF vector for initial guess
-    @time result = optimize(x -> energy_functional(x, s_old, af, xl, mm, parN, parA, parM, Lxx, Lxy, Lyx, Lyy), dof, LBFGS(linesearch=LineSearches.BackTracking()), Optim.Options(x_tol = parN.xTol, f_tol = parN.fTol, g_tol = parN.gTol); autodiff = :forward);
+    @time result = optimize(x -> energy_functional(x, s_old, af, xl, mm, parN, parA, parM, Lxx, Lxy, Lyx, Lyy), dof, LBFGS(m = 20, alphaguess = LineSearches.InitialHagerZhang(α0 = 1.0)), Optim.Options(x_tol = parN.xTol, f_tol = parN.fTol, g_tol = parN.gTol); autodiff = :forward);
     if Optim.converged(result) == 0 # Check convergence
         @printf("Warning: Optimisation did not converge.\n")
     end
