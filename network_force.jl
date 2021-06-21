63ce9592676dd2eb509120b2993904ac023c7918
Alex Tam <alextam@adam.com.au>
2021-06-21 16:48:22 +1000

    Update network_force.jl

--- network_force.jl
+++ network_force.jl
@@ -2,12 +2,12 @@
 # Alex Tam, 12/10/2020
 
 "Compute the contractile/expansive forces in the network"
-function network_force(s, s_old, af, xl, mm, random, parN, parA, parM, Lxx, Lxy, Lyx, Lyy)
+function network_force(s, s_old, af, xl, mm, parN, parA, parM, Lxx, Lxy, Lyx, Lyy)
     x::Vector{Real} = build_dof(s); # Build DOF vector
     # Compute the network force (+ve expansive, -ve contractile)
-    fxx = -ForwardDiff.derivative(a -> energy_functional(x, s_old, af, xl, mm, random, parN, parA, parM, a, Lxy, Lyx, Lyy), Lxx::Real);
-    fxy = -ForwardDiff.derivative(a -> energy_functional(x, s_old, af, xl, mm, random, parN, parA, parM, Lxx, a, Lyx, Lyy), Lxy::Real);
-    fyx = -ForwardDiff.derivative(a -> energy_functional(x, s_old, af, xl, mm, random, parN, parA, parM, Lxx, Lxy, a, Lyy), Lyx::Real);
-    fyy = -ForwardDiff.derivative(a -> energy_functional(x, s_old, af, xl, mm, random, parN, parA, parM, Lxx, Lxy, Lyx, a), Lyy::Real);
+    fxx = -ForwardDiff.derivative(a -> energy_functional(x, s_old, af, xl, mm, parN, parA, parM, a, Lxy, Lyx, Lyy), Lxx::Real);
+    fxy = -ForwardDiff.derivative(a -> energy_functional(x, s_old, af, xl, mm, parN, parA, parM, Lxx, a, Lyx, Lyy), Lxy::Real);
+    fyx = -ForwardDiff.derivative(a -> energy_functional(x, s_old, af, xl, mm, parN, parA, parM, Lxx, Lxy, a, Lyy), Lyx::Real);
+    fyy = -ForwardDiff.derivative(a -> energy_functional(x, s_old, af, xl, mm, parN, parA, parM, Lxx, Lxy, Lyx, a), Lyy::Real);
     return [fxx, fxy, fyx, fyy]
 end
